9a2704f3c402612be3736f0f2fb41bdfc750a375
Rishabhji24 <rishabh.g@iitgn.ac.in>
2021-06-24 23:12:57 +0530

    Added remove functionality

--- lib/Notifications/Notifications.dart
+++ lib/Notifications/Notifications.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:date_format/date_format.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
