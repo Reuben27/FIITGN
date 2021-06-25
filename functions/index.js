fb187df5acdfcb394d87d2e4a6b2a1dd004c1e6c
Student <gautamvashishtha23@gmail.com>
2021-06-25 19:14:26 +0530

    Notifications done

--- functions/index.js
+++ functions/index.js
@@ -6,25 +6,30 @@ exports.scheduledFunctionCrontab = functions.pubsub.schedule("* * * * *")
     .timeZone("Asia/Kolkata")
     .onRun(async (context) => {
         var date = new Date();
-        var d = new Date(date.getTime() + 330 * 60 * 1000);
+        var d = new Date(date.getTime() + 340 * 60 * 1000);
         var min = d.getMinutes();
         var hour = d.getHours();
+        console.log(min);
         database.collection("Notifications").get().then(function (querySnapshot) {
             querySnapshot.forEach(function (doc) {
             const timemap = doc.data()["TimeMap"];
             const numberofnoti = doc.data()["numberofnoti"];
             const tokenid = doc.data()["TokenID"];
+            // console.log("Hello1");
             for(let i=1;i<=numberofnoti;i++){
                 const time = timemap[i.toString()]["time"];
                 // console.log(time);
+                // console.log("Hello2");
                 if ((time["hour"] == hour) && (time["minute"] == min)){
-                    const workoutid = timemap[i.toString()]["workoutid"];
+                    // console.log("Hello3");
+                    const workoutid = timemap[i.toString()]["workoutName"];
+                    // console.log(workoutid);
                     sendNotification(workoutid,tokenid);
                 }
             }
             function sendNotification(workoutid, tokenid) {
                 let title = "Workout Reminder"
-                let body = "You have a workout to be done."
+                let body = "Your workout "+workoutid+" will begin in 10 minutes.";
 
                 const message ={
                     notification : {
